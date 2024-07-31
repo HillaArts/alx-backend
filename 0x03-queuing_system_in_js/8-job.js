73591c448f4b47b5ae95cb5f0dfeffbfdc9bde05
HillaArts <123400794+HillaArts@users.noreply.github.com>
2024-07-31 12:01:25 +0000

    Add function to create push notification jobs with event handling

---
+++ 0x03-queuing_system_in_js/8-job.js
@@ -0,0 +1,31 @@
+import kue from 'kue';
+
+/**
+ * Function to create push notification jobs in a Kue queue
+ * @param {Array} jobs - Array of job objects containing phoneNumber and message
+ * @param {Object} queue - Kue queue instance
+ */
+function createPushNotificationsJobs(jobs, queue) {
+  if (!Array.isArray(jobs)) {
+    throw new Error('Jobs is not an array');
+  }
+
+  jobs.forEach((jobData) => {
+    const job = queue.create('push_notification_code_3', jobData)
+      .save((err) => {
+        if (!err) {
+          console.log(`Notification job created: ${job.id}`);
+        }
+      });
+
+    job.on('complete', () => {
+      console.log(`Notification job ${job.id} completed`);
+    }).on('failed', (err) => {
+      console.log(`Notification job ${job.id} failed: ${err}`);
+    }).on('progress', (progress) => {
+      console.log(`Notification job ${job.id} ${progress}% complete`);
+    });
+  });
+}
+
+export default createPushNotificationsJobs;
